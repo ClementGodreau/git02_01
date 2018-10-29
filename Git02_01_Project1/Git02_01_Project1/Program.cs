e67dc12a19289536ed705ef84ef2d11fbb4a4cc4
clement <clement@example.com>
2018-10-29 09:19:13 +0100

    Version 1

---
+++ Git02_01_Project1/Git02_01_Project1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git02_01_Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Version 1
+        }
+    }
+}
